1c24f42f4bba140100d71f7f4d10e1bc03aed300
Vinícius Marcili <vinipetter@gmail.com>
2024-04-16 21:58:56 -0300

    add ex1 with addEventListener solution

---
+++ ex01.1/ex01/script.js
@@ -0,0 +1,23 @@
+function toggleToDarkTheme() {
+  const body = document.querySelector('body')
+  body.style.backgroundColor = 'black'
+  body.style.color = 'white'
+
+  // OU
+  // document.body.style.backgroundColor = 'black'
+  // document.body.style.color = 'white'
+}
+
+function toggleToLightTheme() {
+  document.body.style.backgroundColor = 'white'
+  document.body.style.color = 'black'
+}
+
+function toggleToGrayTheme() {
+  document.body.style.backgroundColor = 'gray'
+  document.body.style.color = 'white'
+}
+
+document.getElementById('dark').addEventListener('click', toggleToDarkTheme)
+document.getElementById('light').addEventListener('click', toggleToLightTheme)
+document.getElementById('gray').addEventListener('click', toggleToGrayTheme)
